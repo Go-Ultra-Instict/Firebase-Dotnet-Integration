17598fd1bbf77ed001bf4262b77e6eff5ea67b40
Vishnu Kumar PS <vishnu.kumarps@claysys.net>
2023-02-26 14:35:01 +0530

    feat: [Firebase] SignIn

--- Dev-TestMate-Console-App/Program.cs
+++ Dev-TestMate-Console-App/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! ");
+﻿using Firebase.Auth;
+using FirebaseAdmin.Auth;
+
+namespace Dev.Test.Mate.ConsoleApp;
+public class Program
+{
+    private const string API_KEY = "AIzaSyDugDJG7csKUYxCtE0YBL-Fnb57Wdk4A5g";
+
+    static async Task Main(string[] args)
+    {
+        FirebaseAuthProvider firebaseAuthProvider = new FirebaseAuthProvider(new FirebaseConfig(API_KEY));
+
+        //FirebaseAuthLink firebaseAuthLink = await firebaseAuthProvider.CreateUserWithEmailAndPasswordAsync("singletonsean@gmail.com", "test123", "SingletonSean");
+        FirebaseAuthLink firebaseAuthLink = await firebaseAuthProvider.SignInWithEmailAndPasswordAsync("test.vishnukumarps@gmail.com", "vISHNU@1234");
+
+        Console.WriteLine(firebaseAuthLink.FirebaseToken);
+
+        
+    }
+}
+
