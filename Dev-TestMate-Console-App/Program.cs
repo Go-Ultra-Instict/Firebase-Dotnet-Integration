f06341a417b411dc19f0e41f13ab805f4c410b71
Anushree1811 <anushri1811@gmail.com>
2023-03-13 14:04:28 +0530

    temp changes

--- Dev-TestMate-Console-App/Program.cs
+++ Dev-TestMate-Console-App/Program.cs
@@ -4,14 +4,14 @@ using FirebaseAdmin.Auth;
 namespace Dev.Test.Mate.ConsoleApp;
 public class Program
 {
-    private const string API_KEY = "AIzaSyDugDJG7csKUYxCtE0YBL-Fnb57Wdk4A5g";
+    private const string API_KEY = "AIzaSyDsRqHl36anY_xBrPOf1lzKXt4IsSDAVk8";
 
     static async Task Main(string[] args)
     {
         FirebaseAuthProvider firebaseAuthProvider = new FirebaseAuthProvider(new FirebaseConfig(API_KEY));
 
-        //FirebaseAuthLink firebaseAuthLink = await firebaseAuthProvider.CreateUserWithEmailAndPasswordAsync("singletonsean@gmail.com", "test123", "SingletonSean");
-        FirebaseAuthLink firebaseAuthLink = await firebaseAuthProvider.SignInWithEmailAndPasswordAsync("test.vishnukumarps@gmail.com", "vISHNU@1234");
+        //FirebaseAuthLink firebaseAuthLink = await firebaseAuthProvider.CreateUserWithEmailAndPasswordAsync("anushree@gmail.com", "test1234", "anushree");
+        FirebaseAuthLink firebaseAuthLink = await firebaseAuthProvider.SignInWithEmailAndPasswordAsync("anushri1811@gmail.com", "anushree@123");
 
         Console.WriteLine(firebaseAuthLink.FirebaseToken);
 
--- Dev-TestMate-Web-API/Controllers/UserController.cs
+++ Dev-TestMate-Web-API/Controllers/UserController.cs
@@ -2,7 +2,11 @@
 using Dev.TestMate.WebAPI.Models;
 using FirebaseAdmin.Auth;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Http.Headers;
 
 namespace Dev.TestMate.WebAPI.Controllers;
 
@@ -97,5 +101,202 @@ public class UserController : ControllerBase
 
 
     // TODO  Write Signup
+
+
+    [HttpGet]
+    public async Task<object> getUserAsync( string token)
+    {
+        try
+        {
+            //var httpClient = new HttpClient();
+
+            //using (var request = new HttpRequestMessage(new HttpMethod("POST"), "https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=[API_KEY]"))
+            //{
+            //    var requestBody = new
+            //    {
+            //        idToken = token,
+
+            //    };
+
+            //    request.Content = 
+            //    request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
+
+            //    var response = await httpClient.SendAsync(request);
+            //}
+
+            var httpClient = new HttpClient();
+
+            var request = new HttpRequestMessage(new HttpMethod("POST"), "https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyDsRqHl36anY_xBrPOf1lzKXt4IsSDAVk8 ");
+            
+                var requestBody = new
+                {
+                    idToken = token,
+                };
+
+                request.Content = new StringContent(JsonConvert.SerializeObject(requestBody));
+                request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
+
+                var response = await httpClient.SendAsync(request);
+
+            string responseString = await response.Content.ReadAsStringAsync();
+
+            return responseString;
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    var errorMessage = await response.Content.ReadAsStringAsync();
+                    throw new Exception($"Failed to verify user token: {errorMessage}");
+                }
+
+            
+            
+
+            return null;
+        }
+        catch (Exception ex)
+        {
+
+            throw;
+        }
+    }
+    
+
+    [HttpPost("SignUp")]
+    public async Task<string> SignUp(string email, string password)
+    {
+        
+
+        var httpClient = new HttpClient();
+
+        var requestBody = new
+        {
+            email = email,
+            returnSecureToken=true,
+            password = password
+        };
+
+        var request = new HttpRequestMessage(new HttpMethod("POST"), "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDsRqHl36anY_xBrPOf1lzKXt4IsSDAVk8");
+            
+                //request.Content = new StringContent("{\"email\":\"deepti@gmail.com\",\"password\":\"test12345\",\"returnSecureToken\":true}");
+                request.Content = new StringContent(JsonConvert.SerializeObject(requestBody));
+                request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
+
+                var response = await httpClient.SendAsync(request);
+
+        return null;
+            
+        
+    }
+
+
+
+    [HttpPost("SignIn")]
+    public async Task<string> SignIn(string email, string password)
+    {
+
+
+        var httpClient = new HttpClient();
+
+        var requestBody = new
+        {
+            email = email,
+            returnSecureToken = true,
+            password = password
+        };
+
+        var request = new HttpRequestMessage(new HttpMethod("POST"), "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDsRqHl36anY_xBrPOf1lzKXt4IsSDAVk8");
+
+        //request.Content = new StringContent("{\"email\":\"deepti@gmail.com\",\"password\":\"test12345\",\"returnSecureToken\":true}");
+        request.Content = new StringContent(JsonConvert.SerializeObject(requestBody));
+        request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
+
+        var response = await httpClient.SendAsync(request);
+
+        return null;
+
+
+    }
+
+    [HttpPost("SendPasswordResetEmail")]
+    public async Task SendPasswordResetEmail(string email)
+    {
+
+
+        var httpClient = new HttpClient();
+
+        var requestBody = new
+        {
+            email = email,
+            requestType = "PASSWORD_RESET"
+
+        };
+
+        var request = new HttpRequestMessage(new HttpMethod("POST"), $"https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyDsRqHl36anY_xBrPOf1lzKXt4IsSDAVk8");
+              
+
+                request.Content = new StringContent(JsonConvert.SerializeObject(requestBody));
+                request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
+
+                var response = await httpClient.SendAsync(request);
+                response.EnsureSuccessStatusCode();
+            
+        
+
+
+    }
+
+    [HttpPost("IsCodeValid")]
+    public async Task IsCodeValid(string oobCode)
+    {
+
+
+        var httpClient = new HttpClient();
+
+        var requestBody = new
+        {
+            oobCode = oobCode,
+            requestType = "PASSWORD_RESET"
+
+        };
+
+        var request = new HttpRequestMessage(new HttpMethod("POST"), "https://identitytoolkit.googleapis.com/v1/accounts:resetPassword?key=AIzaSyDsRqHl36anY_xBrPOf1lzKXt4IsSDAVk8");
+            
+                request.Content = new StringContent(JsonConvert.SerializeObject(requestBody));
+                request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
+
+                var response = await httpClient.SendAsync(request);
+            
+
+    }
+    
+    
+    
+    
+    [HttpPost("ConfirmPasswordReset")]
+    public async Task ConfirmPasswordReset(string oobCode, string newPassword)
+    {
+
+
+        var httpClient = new HttpClient();
+
+        var requestBody = new
+        {
+            oobCode = oobCode,
+            newPassword = newPassword
+
+        };
+
+        var request = new HttpRequestMessage(new HttpMethod("POST"), "https://identitytoolkit.googleapis.com/v1/accounts:resetPassword?key=AIzaSyDsRqHl36anY_xBrPOf1lzKXt4IsSDAVk8");
+            
+                request.Content = new StringContent(JsonConvert.SerializeObject(requestBody));
+                request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
+
+                var response = await httpClient.SendAsync(request);
+            
+        
+
+
+    }
+
 
 }
